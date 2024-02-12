1b5284487130ac405bf5708366e737c52f669495
CSESHROFF <130203287+CSESHROFF@users.noreply.github.com>
2024-02-12 17:15:51 +0000

    forof

---
+++ iteration/forofloop.js
@@ -0,0 +1,27 @@
+// for of
+
+// const arr = [1,2,3,4,5]
+
+// for (const i of arr) {
+//     console.log(i);
+    
+// }
+
+// const greetings = "hello world"
+// for (const i of greetings) {
+//     console.log(i);
+    
+// }
+
+
+// maps
+
+const map = new Map()
+map.set('in',"india")
+map.set('usa',"america")
+
+console.log(map);
+for (const [key,value] of map) {
+    console.log(key, value);
+    
+}
